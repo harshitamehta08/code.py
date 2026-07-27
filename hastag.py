5dbba8fbd983a7d48d4ba3927cfb16512db6e0fc
Harshita Mehta <mharshita687@gmail.com>
2026-07-27 09:56:21 -0800

    Add files via upload

---
+++ hastag.py
@@ -0,0 +1,45 @@
+for i in range(5):
+   print("harsh")
+
+
+for n in range(1,6):
+    print("n:")
+
+
+for letter in "python":
+    print(letter)
+
+
+for name in "python":
+    print(name)
+
+
+for m in range(2,11,2): #range(start,stop,step)
+    print(m)
+
+
+for h in range(1,11):
+    print(f"{h}x{i}={h*i}")
+
+
+table=int(input("enter your number"))
+print("table of:")
+for i in range(1,11):
+    print(f"{table}x{i}={table*i}")
+
+
+sum=0
+for b in range(1,11):
+    sum+=i
+print(sum)
+
+
+for s in range(2,22,2):
+    print(s)
+
+
+mum=int (input("enter your number"))
+fact=1
+for i in range(1,mum+1):
+    fact*=i
+    print(fact)
