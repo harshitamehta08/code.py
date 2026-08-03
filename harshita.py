155ae77fec12b86d26aff4594e23bf8aeb6f5f8c
Harshita Mehta <mharshita687@gmail.com>
2026-08-03 20:02:05 +0530

    Add files via upload

---
+++ harshita.py
@@ -0,0 +1,41 @@
+#print hello world
+print("hello world")
+
+
+#swapping
+x=3
+y=7
+x,y=y,x
+print("after swap",x,y)
+
+
+#use format 
+name="harsh"
+age=18
+print("name:")
+print("age")
+
+
+#implement all the data type
+int_var=9
+float_var=3.14
+str_var="hello"
+list_var=[1,2,3]
+tuple_var=("hello","world")
+dict_var={"name":"harsh","age":18}
+set_var={1,2,3}
+print("integer:",int_var)
+print("float:",float_var)
+print("string:",str_var)
+print("list:",list_var)
+print("tuple:",tuple_var)
+print("dictionary:",dict_var)
+print("set:",set_var)
+
+
+#variable using a string .format f()
+name="harsh"
+age=18
+print("my name is {} and i am {} years old".format(name,age))
+
+
