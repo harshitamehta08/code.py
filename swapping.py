48daa75c459b8a8de2159d31353ad6e65f792056
Harshita Mehta <mharshita687@gmail.com>
2026-08-17 14:44:29 +0530

    with and without

---
+++ swapping.py
@@ -0,0 +1,22 @@
+# without third variable
+a=10
+b=5
+a,b = b,a
+print("swapping:")
+print(a)
+print(b)
+
+
+
+
+# with third variable
+x=23
+y=67
+z=x
+x=y
+y=z
+print("swapping:")
+print(x)
+print(y)
+
+
