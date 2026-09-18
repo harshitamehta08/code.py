f9b5416263da68f760c16594e40257c31a054fb1
harshitamehta08 <harshitamehtaa08>
2026-09-18 11:48:56 +0530

    control statements

---
+++ control.py
@@ -0,0 +1,24 @@
+# break
+print("\n break")
+for i in range(1,6):
+    if i==3:
+        break
+    print(i)
+
+
+
+# continue
+print("\n",'continue')
+for r in range(1,6):
+    if r==3:
+        continue
+    print(r)
+
+
+
+# pass
+print("\n",'pass')
+for n in range(1,6):
+    if n==3:
+        pass
+    print(n)    
